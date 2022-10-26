52a86e1ec2902d4f211f855e58eef1ea91510133
Daiana Bilbao <dbilbao@cs.colostate.edu>
2022-10-25 19:34:24 -0600

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.MUMBAI_ALCHEMY_API_URL,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+      gas: 2100000,
+      gasPrice: 8000000000
+    }
+  }
+};
+
+export default config;
